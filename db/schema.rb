ac7331865b927e0065307458c055cb527815085e
Priya-sopho <prrani28@gmail.com>
2017-03-28 22:12:43 +0530

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328143158) do
+ActiveRecord::Schema.define(version: 20170328163815) do
 
   create_table "appointments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "patient_id"
@@ -104,6 +104,14 @@ ActiveRecord::Schema.define(version: 20170328143158) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.index ["institution_id"], name: "index_timeslots_on_institution_id", using: :btree
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "type"
+    t.integer  "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "appointments", "patients"
