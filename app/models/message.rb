a34fd076adbf2cc5d0717a505aea0da7c010d905
Priya-sopho <prrani28@gmail.com>
2017-03-29 08:34:11 +0530

    Added action cable left with render

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :body, :conversation_id, :user_id
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
