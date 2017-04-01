cfaa666c89f96a8f6714f9d63091df182bc3fc77
Priya-sopho <prrani28@gmail.com>
2017-04-01 11:36:16 +0530

    modified chat backend

---
+++ app/controllers/v1/base_controller.rb
@@ -0,0 +1,11 @@
+class V1::BaseController < ApplicationController
+private
+
+      def render_api(object, status = :ok)
+        if object.respond_to?(:errors) && object.errors.present?
+          render json: { message: object.errors.full_messages.to_sentence }, status: :unprocessable_entity
+        else
+          render json: object, status: status
+        end
+      end
+end
---
+++ config/initializers/action_cable.rb
@@ -0,0 +1,3 @@
+Rails.application.config.action_cable.disable_request_forgery_protection = true
+
+
