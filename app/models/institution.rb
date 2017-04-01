0d6bed77cc02e0d316caae15dde75a2e424de46b
Priya-sopho <prrani28@gmail.com>
2017-04-01 12:46:57 +0530

    improved image_uploader

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,5 +1,14 @@
 class Institution < ApplicationRecord
+  mount_uploader :image, ImageUploader
   has_secure_password
+
+  validates_processing_of :image
+  validate :image_size_validation
+ 
+private
+  def image_size_validation
+    errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+  end
 
   has_one :location
   has_many :clinicals
